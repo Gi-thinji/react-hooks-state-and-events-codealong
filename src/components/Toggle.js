5a53e69fb2f56369be5992a54c812ef8bafe6d76
Gi-thinji <githinji.ngare@gmail.com>
2023-07-21 17:10:38 +0300

    Added handleClick to Toggle.js to handle ON/OFF functionality

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,41 @@
-import React from "react";
+// import { useState } from "react";
+// import React from "react";
+
+// function Toggle() {
+//   const [isOn,setisOn] = useState(false)
+//   function handleClick(){
+//     // console.log("isOn")
+    
+//     setisOn(!isOn)
+//     // isOn ? ON : OFF
+    
+//   }
+  
+//   const col = isOn ? "red" : "white"
+
+//   return <button style={{background:col}} onClick={()=>{handleClick()}}>{isOn ? "ON" : "OFF"}</button>;
+// }
+
+// export default Toggle;
+
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState('false')
+
+  function switchState(){
+    setIsOn(!isOn)
+  }
+
+  // function currentState(){
+  //   if(isOn = 'off'){
+
+  //   }
+  // }
+  const buttonColor = isOn ? "red" : "white"
+  // style=color:'red'
+
+  return <button style={{background: buttonColor}} onClick={switchState} >{isOn? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
